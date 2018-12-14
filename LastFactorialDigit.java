56c3fbef48dfef6b562ce7570ad6f8385e5aef7e
Zac Clifton <zac16530@gmail.com>
2018-12-14 17:54:24 -0500

    update

--- LastFactorialDigit.java
+++ LastFactorialDigit.java
@@ -5,7 +5,7 @@ public class LastFactorialDigit{
         Scanner input = new Scanner(System.in);
 
         int numTests = input.nextInt();
-        for(int i = 0; i< numTests; i++){
+        /*for(int i = 0; i< numTests; i++){
 
             int num = input.nextInt();
             int cal=1;
@@ -13,6 +13,21 @@ public class LastFactorialDigit{
                 cal *= k;
             }
             System.out.println( cal % 10);
+        }*/
+
+        switch(numTests){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("6");
+                break;
+            default:
+                System.out.println("0");
+                break;
         }
     }
 }
